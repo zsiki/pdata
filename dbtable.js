a39f8c8abddf6813c4bdf95835f16f86185128a9
vivien <potovivien@gmail.com>
2017-06-26 18:41:29 +0200

    Selection is done.

--- dbtable.js
+++ dbtable.js
@@ -20,15 +20,28 @@ $(document).ready(function () {
 				// Parse the JSON data returned.
 				var refrData = JSON.parse(data);
 
-				$("#dbtable").html(refrData.html);
+				$("#dbtableCont").html(refrData.html);
 				if (dialog.mode == "filt") {
 					alert("Database filtered. " + refrData.rowcount + " row(s) returned.")
 					dialog.mode = "";
 				} else {
 					alert("Database refreshed!");					
 				};
-			});
+			});	
+
 	});
+
+	// check all checkboxes with the checkbox in the header
+	var checkAll = $("input:checkbox[name='checkAll']");
+	checkAll.click(function(){
+		if (checkAll.checked){
+			checkAll.checked = false
+			$("input:checkbox").each (function() { this.checked = false})}
+		else {
+			checkAll.checked = true
+			$("input:checkbox").each (function() { this.checked = true})}
+	});
+
 
 	// delete marked records
 	$("#del").click(function () {
@@ -37,6 +50,7 @@ $(document).ready(function () {
 			each(function (ind, val) {
 				s += val.name + ";";
 			});
+		
 		//alert(s);
 		if (s.length) {
 			$.ajax({
@@ -70,18 +84,26 @@ $(document).ready(function () {
 	var d1F = "";
 	var d2F = "";
 
+	// Initialize select dialog box variables.
+	var idS = "";
+	var eastingS = "";
+	var northingS = "";
+	var elevS = "";
+	var d1S = "";
+	var d2S = "";
+	
 	function dialogFill() {
+		// Selected checkboxes.
+		s = $(":input:checkbox:checked");
+		
 		// Reset the dialog box text.
 		$("#filtMessage").html("");
 
 		// Reset the dialog box fields except if filter is selected.
-		if (dialog.mode != "filt") {
+		if (dialog.mode != "filt" || dialog.mode != "sel") {
 			$("#diaFields")[0].reset();
 		};
 
-
-		// Selected checkboxes.
-		s = $(":input:checkbox:checked");
 
 		// Filter mode for dialog box.
 		if (dialog.mode == "filt") {
@@ -99,7 +121,30 @@ $(document).ready(function () {
 								   "<, >, =, <=, >=, and, or, between x and y.</p>" +
 								   "<p style='margin-top:0'>Example: < 2017-02-02 and >= 2017-01-03</p>")
 		};
-
+		
+		// Select mode for dialog box.
+		if (dialog.mode == "sel") {
+			// Checking if any checkboxes are thicked and alert the user
+			if (s.length >= 1){
+				alert("You have already selected row(s)! Clicking 'Save' will delete the current selection!")
+			}
+			
+			// Getting the previous select entries.
+			$("#id").val(idS);
+			$("#easting").val(eastingS);
+			$("#northing").val(northingS);
+			$("#elev").val(elevS);
+			$("#d1").val(d1S);
+			$("#d2").val(d2S);
+		
+			// Show selecting options to the user.
+			$("#filtMessage").html("<p style='margin-bottom:8px'>RegEx can be used to select the point ID's.</p>" +
+								   "<p style='margin-top:0;margin-bottom:8px'>Valid operators for the easting, northing, elevation, date and hour selecting: " +
+								   "<, >, =, <=, >=, and, or, between x and y.</p>" +
+								   "<p style='margin-top:0'>Example: < 2017-02-02 and >= 2017-01-03</p>")
+		};
+		
+		// Update mode for dialog box.
 		if (dialog.mode == "upd") {
 			var s = $(":input:checkbox:checked");
 			if (s.length == 1) {
@@ -152,36 +197,111 @@ $(document).ready(function () {
 
 			// Validate the field values.
 			// Easting, Northing and Elevation validation:
-			if (coordValid(eastingF) == 0) {
-				alert("Easting filter invalid!");
+			if (coordValid(eastingS) == 0) {
+				alert("Easting select invalid!");
 				return;
 			};
 
-			if (coordValid(northingF) == 0) {
-				alert("Northing filter invalid!");
+			if (coordValid(northingS) == 0) {
+				alert("Northing select invalid!");
 				return;
 			};
 
-			if (coordValid(elevF) == 0) {
-				alert("Elevation filter invalid!");
+			if (coordValid(elevS) == 0) {
+				alert("Elevation select invalid!");
 				return;
 			};
 
 			// D1 validation (year-month-day):
-			if (dateValid(d1F) == 0) {
-				alert("Date filter invalid!")
+			if (dateValid(d1S) == 0) {
+				alert("Date select invalid!")
 				return;
 			};
 
 			//D2 validation (hour-minute-second):
-			if (hmsValid(d2F) == 0) {
-				alert("Hour:Minute:Second filter invalid!");
+			if (hmsValid(d2S) == 0) {
+				alert("Hour:Minute:Second select invalid!");
 				return;
 			};
 
 			// Trigger the refresh button with the filter settings.
 			$("#ref").trigger("click");
 		};
+
+		// Select option for dialog box
+		if (dialog.mode == "sel") {
+			// uncheck selected checkboxes
+			$("input:checkbox:checked").each (function() { this.checked = false});
+
+			// Get the values from the select dialog box.
+			idS = $("#id").val();
+			eastingS = $("#easting").val();
+			northingS = $("#northing").val();
+			elevS = $("#elev").val();
+			d1S = $("#d1").val();
+			d2S = $("#d2").val();
+
+			
+		// Validate the field values.
+			// Easting, Northing and Elevation validation:
+			if (coordValid(eastingS) == 0) {
+				alert("Easting filter invalid!");
+				return;
+			};
+
+			if (coordValid(northingS) == 0) {
+				alert("Northing filter invalid!");
+				return;
+			};
+
+			if (coordValid(elevS) == 0) {
+				alert("Elevation filter invalid!");
+				return;
+			};
+
+			// D1 validation (year-month-day):
+			if (dateValid(d1S) == 0) {
+				alert("Date filter invalid!")
+				return;
+			};
+
+			//D2 validation (hour-minute-second):
+			if (hmsValid(d2S) == 0) {
+				alert("Hour:Minute:Second filter invalid!");
+				return;
+			};
+
+
+			// if no conditions are given, uncheck all the checkboxes
+			if (idS + eastingS + northingS + elevS + d1S + d2S == ""){
+				$("input:checkbox:checked").each (function() { this.checked = false})
+				dialog.dialog("close")
+				return
+			};
+
+
+			$.ajax({
+				url: path + "dbtable",
+				data: { 
+					table: "pdata",
+					idFilt: idS,
+					eastingFilt: eastingS,
+					northingFilt: northingS,
+					elevFilt: elevS,
+					d1Filt: d1S,
+					d2Filt: d2S,
+					sel: 1
+				}
+
+			// Check the selected checkboxes.
+			}).done( function (data) {				
+				selData = JSON.parse(data);				
+				selData.forEach( function(val) {
+					$("input:checkbox[name='" + val + "']")[0].checked = true;			
+				})
+			})
+		};
+
 
 		// check fields
 		var loc = { table: "pdata" };
@@ -276,7 +396,7 @@ $(document).ready(function () {
 		};
 	};
 
-	// Validity checker for the hour-minute-second field.
+	// Validity checker for the time field.
 	function hmsValid(d2) {
 		matches = /^(<|>|=|<=|>=){1}\ ?\d{2}\:\d{2}\:\d{2}$/.test(d2) +
 				  /^(<|>|=|<=|>=){1}\ ?\d{2}\:\d{2}\:\d{2}\ (and|or)\ (<|>|=|<=|>=){1}\ ?\d{2}\:\d{2}\:\d{2}$/.test(d2) +
@@ -300,22 +420,25 @@ $(document).ready(function () {
 
 	$("#dia").dialog({ open: dialogFill });
 
+	// Open the data insert dialog if the "Insert" button is pressed.
 	$("#ins").click(function () {
 		dialog.mode = "ins";
 		dialog.dialog("open");
 	});
 
+	// Open the data update dialog if the "Update" button is pressed.
 	$("#upd").click(function () {
 		dialog.mode = "upd";
 		dialog.dialog("open");
 	});
 
+	// Open the data selection dialog if the "Select" button is pressed.
 	$("#sel").click(function () {
 		dialog.mode = "sel";
 		dialog.dialog("open");
 	});
 
-	// Open the data filter dialog if the "filter" button is pressed.
+	// Open the data filter dialog if the "Filter" button is pressed.
 	$("#filt").click(function () {
 		dialog.mode = "filt";
 		dialog.dialog("open");
